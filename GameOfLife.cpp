9a810a4abbd3352e38f71d4632d452a7001f0284
filip <fiqxio@gmail.com>
2017-01-25 23:38:14 +0100

    asd

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -4,7 +4,7 @@
 #include "GameOfLife.h"
 #include <stdlib.h>
 #include <windows.h>
-void fillField(gameField &game_field){
+void fillField(gameField game_field){
     for(int i = 0; i<f_height; i++){
         for(int j = 0; j<f_width; j++){
             game_field[i][j] = 0;
@@ -25,11 +25,11 @@ void drawField(gameField game_field){
     Sleep(200);
 //    std::cout<<"--------------------"<<std::endl;
 }
-void updateCell(gameField &game_field, short x, short y, unsigned short status){
+void updateCell(gameField game_field, short x, short y, unsigned short status){
     game_field[x][y] = status;
 }
-unsigned short getAliveNeighbors(gameField game_field, short x, short y){
-    int count = 0;
+unsigned short int getAliveNeighbors(gameField game_field, short x, short y){
+    unsigned short int count = 0;
     for(int i = std::max<int>(0, x-1); i<=std::min<int>(x+1,f_height); i++){
         for(int j = std::max<int>(0, y-1); j<=std::min<int>(y+1,f_width); j++){
         if(i==x&&j==y)continue;
@@ -38,19 +38,25 @@ unsigned short getAliveNeighbors(gameField game_field, short x, short y){
     }
     return count;
 }
-void generation(gameField &game_field){
-    gameField newGeneration;
-    /** create a mirror of current game_field **/
-    std::memcpy(newGeneration, game_field, sizeof(game_field[0][0])*f_height*f_width);
+void updateField(gameField game_field, gameField newGeneration){
     for(int x=0; x<f_height; x++){
         for(int y=0;y<f_width; y++){
-            unsigned short aliveNeighbours = getAliveNeighbors(game_field, x, y);
+        game_field[x][y]=newGeneration[x][y];
+        }
+    }
+}
+void generation(gameField game_field){
+    gameField newGeneration;
+    /** create a mirror of current game_field **/
+    for(int x=0; x<f_height; x++){
+        for(int y=0;y<f_width; y++){
+            unsigned short int aliveNeighbours = getAliveNeighbors(game_field, x, y);
             if(aliveNeighbours<2)newGeneration[x][y]=false;
             if(aliveNeighbours==3)newGeneration[x][y]=true;
             if(aliveNeighbours>3)newGeneration[x][y]=false;
         }
     }
-    std::memcpy(game_field, &newGeneration, sizeof(newGeneration[0][0])*f_height*f_width);
+    updateField(game_field, newGeneration);
     drawField(game_field);
 }
 
--- GameOfLife.h
+++ GameOfLife.h
@@ -5,8 +5,8 @@ const unsigned short f_width = 20;
 
 typedef unsigned short gameField[f_height][f_width];
 //funkcje
-void fillField(gameField &game_field);
+void fillField(gameField game_field);
 void drawField(gameField game_field);
-void updateCell(gameField &game_field, short x, short y, unsigned short status);
-void generation(gameField &game_field);
+void updateCell(gameField game_field, short x, short y, unsigned short status);
+void generation(gameField game_field);
 #endif // GAMEOFLIFE_H_INCLUDED
